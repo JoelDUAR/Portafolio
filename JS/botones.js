3fdbc88add9690037c97fe037f756dbbebd3e033
Joel Dupraz <joel_dupraz@hotmail.com>
2022-06-29 17:48:31 -0300

    menu completado, con funcionalidad de botones

--- JS/botones.js
+++ JS/botones.js
@@ -1,5 +1,81 @@
 let botonNav = document.querySelector(".lbl-menu");
+let menu = document.querySelector(".contenedor__menu");
+let btnChecked = document.querySelector("#btn-menu");
+let altura = menu.offsetTop;
+let btnSobreMi = document.querySelector(".btn1");
+let btnSoftSkills = document.querySelector(".btn2");
+let btnFormacion = document.querySelector(".btn3");
+let btnProyectos = document.querySelector(".btn4");
+let btnContacto = document.querySelector(".btn5");
 
-/* botonNav.addEventListener("click", function(){
-    
-}) */
+
+btnSobreMi.addEventListener("click", function () {
+    if (btnChecked.checked == true) {
+        btnChecked.checked = false
+        menu.classList.remove("menu-fixed");
+        cerrarMenu();
+      }
+      window.location.href = "#container__imagen__sobre-mi"
+  });
+
+btnSoftSkills.addEventListener("click", function () {
+    if (btnChecked.checked == true) {
+        btnChecked.checked = false
+        menu.classList.remove("menu-fixed");
+        cerrarMenu();
+      }
+      window.location.href = "#container__softskills"
+});
+
+btnFormacion.addEventListener("click", function () {
+    if (btnChecked.checked == true) {
+        btnChecked.checked = false
+        menu.classList.remove("menu-fixed");
+        cerrarMenu();
+      }
+      window.location.href = "#container__formacion"
+});
+
+btnProyectos.addEventListener("click", function () {
+    if (btnChecked.checked == true) {
+        btnChecked.checked = false
+        menu.classList.remove("menu-fixed");
+        cerrarMenu();
+      }
+      window.location.href = "#container__proyectos"
+});
+
+btnContacto.addEventListener("click", function () {
+    if (btnChecked.checked == true) {
+        btnChecked.checked = false
+        menu.classList.remove("menu-fixed");
+        cerrarMenu();
+      }
+      window.location.href = "#container__formulario"
+});
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= altura) {
+    menu.classList.add("menu-fixed");
+  } else {
+    menu.classList.remove("menu-fixed");
+  }
+});
+
+botonNav.addEventListener("click", function () {
+  if (btnChecked.checked == false) {
+    console.log(btnChecked.checked)
+    abrirMenu();
+  } else {
+    console.log(btnChecked.checked)
+    cerrarMenu();
+  }
+});
+
+function abrirMenu() {
+  menu.classList.remove("invisible");
+}
+
+function cerrarMenu() {
+  menu.classList.add("invisible");
+}
